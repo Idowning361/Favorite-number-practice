b1df2382f404929fb125509ba9945a0b9923a65d
Idowning361 <150697892+Idowning361@users.noreply.github.com>
2024-10-07 18:01:59 -0500

    Add files via upload

---
+++ Favorite number.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+
+    int FavNumb;
+
+        cout<<"Hi welcome to my program what's your favorite number?"<<endl;
+            cin>>FavNumb;
+        cout<<"Wow that's funny "<<FavNumb<<" is my favorite number too!"<<endl;
+
+
+
+    return 0;
+}
